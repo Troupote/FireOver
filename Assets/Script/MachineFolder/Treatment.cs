af8be4cd843b4c59388d6f2535df4e16696cb226
Troupote <73849862+Troupote@users.noreply.github.com>
2024-07-07 11:17:44 +0200

    treatment v2

--- Assets/Script/MachineFolder/Treatment.cs
+++ Assets/Script/MachineFolder/Treatment.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
-public class Treatment : MonoBehaviour
+public class Treatment : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<GameObject> Queue = new List<GameObject>();
+    private IEnumerator enumerator;
+
+
+    public void Start()
     {
-        
+        enumerator = Treat();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddQueue(GameObject Item)
     {
-        
+        Queue.Add(Item);
+    }
+
+    public IEnumerator Treat()
+    {
+        if (Queue.Count > 0)
+        {
+            GameObject Temp = Queue[0];
+
+            Queue.RemoveAt(0);
+
+            yield return new WaitForSeconds(2);
+
+            GameObject ObjectToSpawn = Instantiate(Temp,transform.position + new Vector3(0, 0, 4f), Quaternion.identity);
+
+        }
     }
 }
