09967aae91a9810b662634da8b30b16e4e661086
Walpurgis Time <122738788+WalpurgisTime@users.noreply.github.com>
2024-07-11 15:06:42 +0200

    UI fixed

--- Assets/Script/Loot/loot_item.cs
+++ Assets/Script/Loot/loot_item.cs
@@ -19,7 +19,7 @@ public class loot_item : MonoBehaviour
     {
         for (int i = 0; i<30 ;i++)
         {
-            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-10,10),0,UnityEngine.Random.Range(-10,10));
+            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-10,10),2,UnityEngine.Random.Range(-10,10));
             GameObject newLoots = Instantiate(lootPrefab[i%(lootPrefab.Length)], randomPosition, Quaternion.identity);
         }
     }
